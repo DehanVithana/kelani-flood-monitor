d79b6ad782c1f42c6a1b3793b42ba14463277e35
DehanVithana <dehan.m.vithana@gmail.com>
2025-12-01 09:25:47 +0530

    Create constants.js

---
+++ src/utils/constants.js
@@ -0,0 +1,17 @@
+export const NAGALAGAM_STATION = {
+  name: 'Nagalagam Street',
+  river: 'Kelani Ganga',
+  latitude: 6.9271,
+  longitude: 79.8612,
+  thresholds: {
+    normal: 2.0,
+    alert: 2.0,
+    minorFlood: 2.5,
+    majorFlood: 3.0
+  }
+}
+
+export const API_ENDPOINTS = {
+  github: 'https://raw.githubusercontent.com/nuuuwan/lk_irrigation/main/data/rwlds',
+  backup: 'https://www.arcgis.com/apps/dashboards/2cffe83c9ff5497d97375498bdf3ff38'
+}
